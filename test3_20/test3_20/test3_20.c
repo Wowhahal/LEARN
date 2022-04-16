d0680a46c860d995fd5b46ee418c0d5eb0cc09b1
Wowhahal <969981419@qq.com>
2022-04-16 14:08:52 +0800

    提交代码

---
+++ test3_20/test3_20/test3_20.c
@@ -0,0 +1,99 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <string.h>
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include<time.h>
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum=Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "bit";
+//	char arr2[] = "##########";//  \0是字符串的结束标志
+//	strcpy(arr2, arr1);
+//	printf("%s\n",arr2);
+//	//strlen - string length 字符串长度
+//	return 0;
+//}
+
+//memset
+//内存—memsery
+//int main()
+//{
+//	char arr[] = "hello world!";
+//	memset(arr, '+', 5);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max=get_max(a, b);
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+//void swap1(int x, int y)
+//{
+//	int tmp = 0;
+//	tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+
+//void swap2(int* pa, int* pb)
+//{
+//	int tmp = 0;
+//	tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("%d %d\n", a, b);
+//	//swap1(a, b);
+//	swap2(&a, &b);
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;//a的地址
+//	*pa = 20;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
