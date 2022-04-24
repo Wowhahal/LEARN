88461dcdbb1c82155a188f4363ae2fe005a8a960
Wowhahal <969981419@qq.com>
2022-04-24 21:28:35 +0800

    提交代码

---
+++ test4_11/test4_11/test4_11.c
@@ -0,0 +1,201 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n % 2 == 1)
+//		{
+//			count++;
+//		}
+//		n = n / 2;
+//	}
+//	return count;
+//}
+
+//int count_bit_one(int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((n >> i) & 1 == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//
+//int count_bit_one(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	scanf("%d", &input);
+//	//写一个函数求二进制表示中有几个1
+//	int count = count_bit_one(input);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+
+//int get_diff_bit(int m, int n)
+//{
+//	int tmp = m ^ n;
+//	return count_bit_one(tmp);
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	int count = get_diff_bit(m, n);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+
+//void print(int m)
+//{
+//	int i = 0;
+//	printf("奇数位为：\n");
+//	for (i = 30 ; i >= 0; i-=2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//	printf("\n");
+//	for (i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	
+//	return 0;
+//}
+
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr,sz);
+//	return 0;
+//}
+
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-3d ", i, j, i * j);
+//
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//	return 0;
+//}
+//#include <string.h>
+//
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//void reverse_string(char arr[])
+//{
+//	int left = 0;
+//	int right = my_strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+
+//void reverse_string(char* arr)
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//	if(my_strlen(arr+1)>=2)
+//		reverse_string(arr + 1);
+//	arr[len - 1] = tmp;
+//
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+int DigitSum(unsigned int num)
+{
+	if (num > 9)
+	{
+		return DigitSum(num / 10) + num % 10;
+
+	}
+	else
+	{
+		return num;
+	}
+}
+
+int main()
+{
+	unsigned int num = 0;
+	scanf("%d", &num);//1729
+	int ret = DigitSum(num);
+	printf("%d\n", ret);
+	return 0;
+}
