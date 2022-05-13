5b9e011c73ffaeb87a9475d711c76faf34cc0abd
Wowhahal <969981419@qq.com>
2022-05-13 20:09:28 +0800

    提交代码

---
+++ test5_1/test5_1/test5_1.c
@@ -0,0 +1,151 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+
+//void reverse(char* left, char* right)
+//{
+//	assert(left != NULL);
+//	assert(right != NULL);
+//
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//void left_move(char* arr, int k)
+//{
+//	assert(arr);
+//
+//	int len = strlen(arr);
+//	assert(k < len);
+//	reverse(arr, arr + k - 1);//逆序左边
+//	reverse(arr + k, arr + len - 1);//逆序右边
+//	reverse(arr, arr + len - 1);//逆序整体
+//}
+
+//int is_left_move(char* s1, char* s2)
+//{
+//	int len = strlen(s1);
+//	int i = 0;
+//	for (i = 0; i < len; i++)
+//	{
+//		left_move(s1, 1);
+//		int ret = strcmp(s1, s2);
+//		if (ret == 0)
+//			return 1;
+//	}
+//	return 0;
+//}
+
+//int is_left_move(char* str1, char* str2)
+//{
+//	int len1 = strlen(str1);
+//	int len2 = strlen(str2);
+//	if (len1 != len2)
+//		return 0;
+//	//1.在str1字符串中追加一个str1字符串
+//	//strcat(str1, str1);
+//	strncat(str1, str1, len1);
+//	//2.判断str2指向的字符串是否是str1指向的字符串子串
+//	//strstr - 找子串
+//	char* ret = strstr(str1, str2);
+//	if (ret == NULL)
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		return 1;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr1[30] = "abcdef";
+//	char arr2[] = "cdefab";
+//	int ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//		printf("Yes\n");
+//	else
+//		printf("No\n");
+//	return 0;
+//}
+
+//杨氏矩阵
+//一个数字矩阵，矩阵的每行从左到右是递增的，从上到下是递增的
+
+
+//int FindNum(int arr[3][3], int k, int row, int col)
+//{
+//	int x = 0;
+//	int y = col - 1;
+//	while (x <= col - 1 && y >= 0)
+//	{
+//		if (arr[x][y] > k)
+//		{
+//			y--;
+//		}
+//		else if (arr[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			//printf("下标是%d %d\n", x, y);
+//			return 1;
+//		}
+//	}
+//	return 0;
+//	
+//}
+
+int FindNum(int arr[3][3], int k, int* px, int* py)
+{
+	int x = 0;
+	int y = *py - 1;
+	while (x <= *px - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
+	}
+	return 0;
+
+}
+
+
+int main()
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 7;
+	int x = 3;
+	int y = 3;
+	//返回型参数
+	int ret = FindNum(arr, k, &x, &y);
+	if (ret == 1)
+	{
+		printf("找到了\n");
+		printf("下标是%d %d\n", x, y);
+	}
+	else
+		printf("找不到\n");
+	return 0;
+}
