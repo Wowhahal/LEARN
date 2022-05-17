849de5d8ff28fb9a797d4a195ae29213d9ecea5e
Wowhahal <969981419@qq.com>
2022-05-17 23:03:56 +0800

    提交代码

---
+++ ContactFile/ContactFile/contact.c
@@ -0,0 +1,281 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "contact.h"
+#include <string.h>
+#include <stdio.h>
+
+
+//void InitContact(struct Contact* ps)
+//{
+//	memset(ps->data, 0, sizeof(ps->data));
+//	ps->size = 0;//设置通讯录最初只有零个元素
+//}
+
+void InitContact(struct Contact* ps)
+{
+	ps->data = (struct PeoInfo*)malloc( DEFAULT_SZ * sizeof(struct PeoInfo));
+	if (ps == NULL)
+	{
+		return;
+	}
+	ps->size = 0;
+	ps->capacity = DEFAULT_SZ ;
+	//把文件中已经存放的通讯录信息，加载到通讯录中
+	LoadContact(ps);
+}
+
+//声明函数
+void CheckCapacity(struct Contact* ps);
+
+void LoadContact(struct Contact* ps)
+{
+	struct PeoInfo tmp = { 0 };
+	FILE* Pfread = fopen("contact.dat", "rb");
+	if (Pfread == NULL);
+	{
+		printf("LoadContact:%s\n", strerror(errno));
+		return;
+	}
+
+	//读取文件，存放到通讯录中
+	while (fread(&tmp, sizeof(struct PeoInfo), 1, Pfread))
+	{
+		CheckCapacity(ps);
+		ps->data[ps->size] = tmp;
+		ps->size++;
+
+	}
+
+	fclose(Pfread);
+	Pfread = NULL;
+
+}
+
+void CheckCapacity(struct Contact* ps)
+{
+	if (ps->size == ps->capacity)
+	{
+		//增容
+		struct PeoInfo* ptr = realloc(ps->data, (ps->capacity + 2) * sizeof(struct PeoInfo));
+		if (ptr != NULL)
+		{
+			ps->data = ptr;
+			ps->capacity += 2;
+			printf("增容成功\n");
+		}
+		else
+		{
+			printf("增容失败\n");
+		}
+	
+	}
+
+}
+
+void AddContact(struct Contact* ps)
+{
+	//检测当前通讯录的容量
+	//1、如果满了 增加空间
+	//2、如果不满 啥也不干
+	CheckCapacity(ps);
+	//增加数据
+		printf("请输入名字：");
+		scanf("%s", ps->data[ps->size].name);
+		printf("请输入年龄：");
+		scanf("%d", &(ps->data[ps->size]).age);
+		printf("请输入性别：");
+		scanf("%s", ps->data[ps->size].sex);
+		printf("请输入电话：");
+		scanf("%s", ps->data[ps->size].tele);
+		printf("请输入地址：");
+		scanf("%s", ps->data[ps->size].addr);
+		ps->size++;
+		printf("添加成功！\n");
+
+
+	//if (ps->size == MAX)
+	//{
+	//	printf("通讯录已满，无法增加！\n");
+	//}
+	//else
+	//{
+	//	printf("请输入名字：");
+	//	scanf("%s", ps->data[ps->size].name);
+	//	printf("请输入年龄：");
+	//	scanf("%d", &(ps->data[ps->size]).age);
+	//	printf("请输入性别：");
+	//	scanf("%s", ps->data[ps->size].sex);
+	//	printf("请输入电话：");
+	//	scanf("%s", ps->data[ps->size].tele);
+	//	printf("请输入地址：");
+	//	scanf("%s", ps->data[ps->size].addr);
+	//	ps->size++;
+	//	printf("添加成功！\n");
+	//}
+}
+
+void ShowContact(const struct Contact* ps)
+{
+	if (ps->size == 0)
+	{
+		printf("通讯录为0\n");
+	}
+	else
+	{
+		int i = 0;
+		//标题
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");
+		for (i = 0; i < ps->size; i++)
+		{
+			//数据
+			printf("%-20s\t%-4d\t%-5s\t%-12s\t%-20s\n",
+				ps->data[i].name,
+				ps->data[i].age,
+				ps->data[i].sex,
+				ps->data[i].tele,
+				ps->data[i].addr
+			);
+		}
+	}
+
+}
+
+//修饰局部变量
+//修饰全局变量
+//修饰函数
+
+static int FindByName(const struct Contact* ps, char name[MAX_NAME])
+{
+	int i = 0;
+	for (i = 0; i < ps->size; i++)
+	{
+		if (0 == strcmp(ps->data[i].name, name))
+		{
+			return i;
+		}
+
+	}
+	return -1;//找不到的情况
+}
+
+void DelContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要删除人的名字：");
+	scanf("%s", name);
+	//查找要删除的人在什么位置
+	//找到了返回名字所在元素的下标
+	//找不到返回-1
+	int pos = FindByName(ps, name);
+	
+
+	//删除
+	if (pos == -1)
+	{
+		printf("要删除的人不存在\n");
+
+	}
+	else
+	{
+		int j = 0;
+		for (j = pos; j <ps->size-1 ; j++)
+		{
+			ps->data[j] = ps->data[j + 1];
+		}
+	}
+	ps->size--;
+	printf("删除成功！\n");
+}
+
+void SearchContact(const struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要查找人的信息：");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("查找的人不存在！\n");
+
+	}
+	else
+	{
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");
+			//数据
+		printf("%-20s\t%-4d\t%-5s\t%-12s\t%-20s\n",
+			ps->data[pos].name,
+			ps->data[pos].age,
+			ps->data[pos].sex,
+			ps->data[pos].tele,
+			ps->data[pos].addr);
+	}
+
+
+}
+
+void ModifyContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入修改人的名字：");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("要修改的人不存在！\n");
+
+	}
+	else
+	{
+		printf("请输入名字：");
+		scanf("%s", ps->data[pos].name);
+		printf("请输入年龄：");
+		scanf("%d", &(ps->data[pos]).age);
+		printf("请输入性别：");
+		scanf("%s", ps->data[pos].sex);
+		printf("请输入电话：");
+		scanf("%s", ps->data[pos].tele);
+		printf("请输入地址：");
+		scanf("%s", ps->data[pos].addr);
+		printf("修改成功！\n");
+	}
+}
+
+void SortContact(struct Contact* ps)
+{
+	int i = 0;
+	for (i = 0; i < ps->size - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < ps->size - 1 - i; j++)
+		{
+			if ( strcmp(ps->data[j].name, ps->data[j + 1].name)>0)
+			{
+				struct PeoInfo str = ps->data[j];
+				ps->data[j] = ps->data[j + 1];
+				ps->data[j + 1] = str;
+			}
+		}
+	}
+}
+
+void DestoryContact(struct Contact* ps)
+{
+	free(ps->data);
+	ps->data = NULL;
+}
+
+
+void SaveContact(struct Contact* ps)
+{
+	FILE* pfWrite = fopen("contact.dat", "wb");
+	if (pfWrite == NULL)
+	{
+		printf("SaveContact:%s\n", strerror(errno));
+		return;
+	}
+	int i = 0;
+	for (i = 0; i < ps->size; i++)
+	{
+		fwrite(&(ps->data[i]), sizeof(struct PeoInfo), 1, pfWrite);
+	}
+}
---
+++ ContactFile/ContactFile/contact.h
@@ -0,0 +1,67 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+// #define MAX 1000
+
+#define DEFAULT_SZ 3
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+
+#include <stdlib.h>
+#include <errno.h>
+
+
+
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT,
+	SAVE
+};
+
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo *data;//存放一个信息
+	int size;//记录当前已经有的元素个数
+	int capacity;//当前通讯录的最大容量
+};
+
+//声明函数
+//通讯录初始化
+void InitContact(struct Contact* ps);
+//添加信息
+void AddContact(struct Contact* ps);
+//信息展示
+void ShowContact(const struct Contact* ps);
+//删除指定的联系人
+void DelContact(struct Contact* ps);
+//查找联系人
+void SearchContact(const struct Contact* ps);
+//修改指定联系人
+void ModifyContact(struct Contact* ps);
+//排序算法
+void SortContact(struct Contact* ps);
+//释放开辟的动态内存
+void DestoryContact(struct Contact* ps);
+//保存已经创建的信息
+void SaveContact(struct Contact* ps);
+//加载文件中的信息
+void LoadContact(struct Contact* ps);
