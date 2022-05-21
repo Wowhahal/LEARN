315b26c1b479db9a2e585b77a3b7bbbcf713e813
Wowhahal <969981419@qq.com>
2022-05-21 22:24:44 +0800

    提交代码

---
+++ test5_21/test5_21/test5_21.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+ 
+#define MAX 100
+
+//int main()
+//{
+//	//printf("%s\n", __FILE__);
+//	//printf("%d\n", __LINE__);
+//	//printf("%s\n", __DATE__);
+//	//printf("%s\n", __TIME__);
+//
+//	int i = 0;
+//	int arr[10] = { {0} };
+//	FILE* pf = fopen("log.txt", "w");
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		fprintf(pf, "file:%s line:%d date:%s time:%s i=%d\n",
+//			__FILE__, __LINE__, __DATE__, __TIME__, i);
+//		printf("%s\n", __FUNCTION__);
+//
+//
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int  main()
+//{
+//	printf("%d\n", __STDC__);
+//	reutrn 0;
+//}
+
+//95% --C
+//99%-- gcc
+
+#define MAX 100
+#define STR "hehe"
+#define reg gerister
+#define do_forever for (;;)
+int main()
+{
+
+	//do_forev er;
+	for (;;);
+
+	//int max=MAX;
+	//printf("%d\n", max);
+	//printf("%s\n", STR);
+
+	return 0;
+}
